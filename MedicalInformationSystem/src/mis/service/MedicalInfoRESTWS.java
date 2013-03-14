50d80e4c88658f92f49d3638423c1c5c2e7de88f
mandarmunshi <mandarmunshi@gmail.com>
2013-03-14 02:45:57 +0000

    Initial Commit

---
+++ MedicalInformationSystem/src/mis/service/MedicalInfoRESTWS.java
@@ -0,0 +1,18 @@
+package mis.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//@Path("/medicalinfoservice")
+public class MedicalInfoRESTWS {
+	
+	//@GET
+	//@Produces(MediaType.TEXT_PLAIN)
+	public String sayHello() {
+		return "Hello World";
+	}
+	
+	
+}
