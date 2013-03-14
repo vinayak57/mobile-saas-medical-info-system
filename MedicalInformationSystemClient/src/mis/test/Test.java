fae812103d6d8529d4bbf21b91b8f6f4c0e5a714
mandarmunshi@gmail.com <mandarmunshi@gmail.com@b7e8ca5a-32b8-7684-79e6-31e713d3c324>
2013-03-14 02:45:57 +0000

    Initial Commit

---
+++ MedicalInformationSystemClient/src/mis/test/Test.java
@@ -0,0 +1,30 @@
+package mis.test;
+
+import java.net.URI;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+public class Test {
+  public static void main(String[] args) {
+    ClientConfig config = new DefaultClientConfig();
+    Client client = Client.create(config);
+    WebResource service = client.resource(getBaseURI());
+    // Get XML
+    //System.out.println(service.path("rest").path("user").accept(MediaType.TEXT_XML).get(String.class));
+    // Get XML for application
+    System.out.println(service.path("rest").path("user").accept(MediaType.APPLICATION_JSON).get(String.class));
+    // Get JSON for application
+    System.out.println(service.path("rest").path("user").accept(MediaType.APPLICATION_XML).get(String.class));
+  }
+
+  private static URI getBaseURI() {
+    return UriBuilder.fromUri("http://localhost:8080/MedicalInformationSystem").build();
+  }
+
+} 
