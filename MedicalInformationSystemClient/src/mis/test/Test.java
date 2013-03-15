020eeead4f680a99646315ae8134984b685a8ae5
mandarmunshi@gmail.com <mandarmunshi@gmail.com@b7e8ca5a-32b8-7684-79e6-31e713d3c324>
2013-03-15 04:32:46 +0000

    user details

--- MedicalInformationSystemClient/src/mis/test/Test.java
+++ MedicalInformationSystemClient/src/mis/test/Test.java
@@ -11,20 +11,50 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class Test {
-  public static void main(String[] args) {
-    ClientConfig config = new DefaultClientConfig();
-    Client client = Client.create(config);
-    WebResource service = client.resource(getBaseURI());
-    // Get XML
-    //System.out.println(service.path("rest").path("user").accept(MediaType.TEXT_XML).get(String.class));
-    // Get XML for application
-    System.out.println(service.path("rest").path("user").accept(MediaType.APPLICATION_JSON).get(String.class));
-    // Get JSON for application
-    System.out.println(service.path("rest").path("user").accept(MediaType.APPLICATION_XML).get(String.class));
-  }
+	static ClientConfig config;
+	static Client client;
+	static WebResource service;
 
-  private static URI getBaseURI() {
-    return UriBuilder.fromUri("http://localhost:8080/MedicalInformationSystem").build();
-  }
+	public Test() {
+		config = new DefaultClientConfig();
+		client = Client.create(config);
+		service = client.resource(getBaseURI());
+	}
 
-} 
+	public static void getAllUsers()
+	{
+		// Get XML
+		// System.out.println(service.path("rest").path("user").accept(MediaType.TEXT_XML).get(String.class));
+		// Get XML for application
+		System.out.println(service.path("rest").path("user")
+				.accept(MediaType.APPLICATION_JSON).get(String.class));
+		// Get JSON for application
+		System.out.println(service.path("rest").path("user")
+				.accept(MediaType.APPLICATION_XML).get(String.class));
+	}
+	
+	public static void getUserByUsername(String username)
+	{
+		String servicePath = "user/" + username; 
+		
+		//Get XML
+		// System.out.println(service.path("rest").path("user").accept(MediaType.TEXT_XML).get(String.class));
+		// Get XML for application
+		System.out.println(service.path("rest").path(servicePath)
+				.accept(MediaType.APPLICATION_JSON).get(String.class));
+		// Get JSON for application
+		System.out.println(service.path("rest").path(servicePath)
+				.accept(MediaType.APPLICATION_XML).get(String.class));
+	}
+	
+	public static void main(String[] args) {
+		//getAllUsers();
+		getUserByUsername("mandar");
+	}
+
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri(
+				"http://localhost:8080/MedicalInformationSystem").build();
+	}
+
+}
